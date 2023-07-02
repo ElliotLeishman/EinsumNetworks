71b6764db5f8cdf908583c664cf834008b111a04
ElliotLeishman <elliot.leishman@gmail.com>
2023-07-02 17:30:29 +0100

    Use Python 3.10 and explicitly install dependencies

--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 rm -rf venv/
-virtualenv --system-site-packages -p python3.6 ./venv
+virtualenv --system-site-packages -p python3.10 ./venv
 source ./venv/bin/activate
 pip3 install --upgrade pip
-pip3 install -r requirements.txt
+python -m pip install torch
+python -m pip install numpy
+python -m pip install pillow
+python -m pip install scipy
+python -m pip install torch-fidelity
+python -m pip install torchvision
+python -m pip install torchmetrics
 cd src
 python datasets.py
