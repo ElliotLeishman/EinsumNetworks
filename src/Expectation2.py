6efe2947eb2e9220af8bdd01d80803a8bdd502c8
ElliotLeishman <elliot.leishman@gmail.com>
2023-07-11 20:04:21 +0100

    Training model

--- src/Expectation2.py
+++ src/Expectation2.py
@@ -1,2 +1,7 @@
 # Script which will train a better model and then return an image
 # of the expectation
+
+
+train_model('../models/',[7], EinsumNetork.NormalArray, K = 15, pd_pieces = 10, fashion_mnist = False, num_epochs = 20, batch_size = 10)
+
+
