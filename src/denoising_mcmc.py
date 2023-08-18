7790363716e3dfe6f543929db3871662990565ae
ElliotLeishman <elliot.leishman@gmail.com>
2023-08-18 21:31:28 +0100

    denoising mcmc3

--- src/denoising_mcmc.py
+++ src/denoising_mcmc.py
@@ -76,8 +76,6 @@ for sig2 in sig2s:
                 # Initialise recording of sample summary statistics after burnin period
                 post_meanvar = ULA.welford(X)
                 absfouriercoeff = ULA.welford(torch.fft.fft2(X).abs())
-                if j % 250 == 0:
-                    print(j,psnr(current_mean.detach(),true_image))
 
             elif j > burnin:
                 # update the sample summary statistics
