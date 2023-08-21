dc14814fc63dce3284ab44ac2f87c651246dc429
ElliotLeishman <elliot.leishman@gmail.com>
2023-08-21 22:25:07 +0100

    inpainting10?

--- src/inpainting.py
+++ src/inpainting.py
@@ -14,7 +14,7 @@ import time as time
 
 sig2s = [0.0001,0.0005,0.001,0.005,0.01,0.05,0.1]
 classes_list = [[7], None]
-num_sam = 1000
+num_sam = 10
 psnr = PeakSignalNoiseRatio(data_range = 1, reduction = 'none', dim = [1,2,3])
 
 # Load the A matrix
@@ -47,8 +47,7 @@ def inpaint_depaint(classes_list, sig2s):
         train_x, train_labels, test_x, test_labels = datasets.load_mnist()
         train_x /= 255.
         test_x /= 255.
-        train_x -= .5
-        test_x -= .5
+
 
         # Select the right class
         if classes is not None:
@@ -93,15 +92,15 @@ def inpaint_depaint(classes_list, sig2s):
             # Remove the noise
             denoised_images = torch.zeros((num_sam,784))
             for j in range(num_sam):
-                denoised_images[j,:] = expectation.depainting(model, As[j], noisy_images[j], K = 15, sigma = sig2)
+                denoised_images[j,:] = expectation.depainting2(model, As[j], noisy_images[j], K = 15, sigma = sig2)
 
             denoised_images = torch.reshape(denoised_images, (-1,1,28,28))
             print(f'Inpainted images have been restored. The shape is {denoised_images.shape}')
 
 
             # Update Summary statistics
-            psnr1 = psnr(denoised_images[:num_sam,:] + 0.5,test_x[:num_sam,:] + 0.5)
-            psnr2 = psnr(noisy_images[:num_sam,:] + 0.5,test_x[:num_sam,:] + 0.5)
+            psnr1 = psnr(denoised_images[:num_sam,:],test_x[:num_sam,:])
+            psnr2 = psnr(noisy_images[:num_sam,:],test_x[:num_sam,:])
 
             means.append(f'sig2 = {sig2}: {torch.mean(psnr1):.2f}')
             sds.append(f'sig2 = {sig2}: {torch.std(psnr1):.2f}')
@@ -111,12 +110,12 @@ def inpaint_depaint(classes_list, sig2s):
             print(means, sds, means2, sds2)
 
             # Create the directories to save the images
-            utils.mkdir_p(f'../experiments/inpainting50/painted/')
-            utils.mkdir_p(f'../experiments/inpainting50/depainted/')
+            utils.mkdir_p(f'../experiments/inpainting_prop/painted/')
+            utils.mkdir_p(f'../experiments/inpainting_prop/depainted/')
 
             # Save the noisy and denoised images as raw tensors
-            torch.save(noisy_images[:num_sam,:], f'../experiments/inpainting50/painted/mnist_{i}_sigma_{sig2}.pt')
-            torch.save(denoised_images[:num_sam,:], f'../experiments/inpainting50/depainted/mnist_{i}_sigma_{sig2}.pt')
+            torch.save(noisy_images[:num_sam,:], f'../experiments/inpainting_prop/painted/mnist_{i}_sigma_{sig2}.pt')
+            torch.save(denoised_images[:num_sam,:], f'../experiments/inpainting_prop/depainted/mnist_{i}_sigma_{sig2}.pt')
 
 
         print(f'Summary statistics for PSNR Values, (denoised & true) (mnist_{i}) means: {means}, sds: {sds}')
